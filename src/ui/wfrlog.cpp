f0ec6bbf1d3a6246b636efd95434f3537df3acda
safocl <safocl88@gmail.com>
2020-06-18 22:02:23 +0400

    add multiple log entityes

--- src/ui/wfrlog.cpp
+++ src/ui/wfrlog.cpp
@@ -1,9 +1,13 @@
 #include "wfrlog.hpp"
+#include "gtkmm/enums.h"
+#include "gtkmm/object.h"
+#include "sigc++/functors/mem_fun.h"
+#include <iterator>
 
 namespace core::ui {
 
 WFrLog::WFrLog() :
-btnAdd( " +++ " ), grid(), logEntity() {
+btnAdd( " +++ " ), grid(), logEntityArr() {
     set_label( Label );
 
     set_margin_bottom( 3 );
@@ -11,13 +15,28 @@ btnAdd( " +++ " ), grid(), logEntity() {
     set_margin_left( 3 );
     set_margin_right( 3 );
 
-    grid.attach( logEntity, 1, 1, 3, 1 );
+    logEntityArr.push_back( Gtk::make_managed<LogEntity>());
+
+    grid.attach( *logEntityArr.at(0), 1, 1, 3, 1 );
     grid.attach( btnAdd, 2, 2 );
 
     add( grid );
     show_all();
+
+    btnAdd.signal_clicked().connect(sigc::mem_fun(*this, &WFrLog::onBtnClicked));
 }
 WFrLog::~WFrLog() {}
 Glib::ustring WFrLog::getName() const { return Label; }
 
+
+
+void WFrLog::onBtnClicked(){
+
+    grid.insert_next_to(btnAdd, Gtk::POS_TOP);
+    logEntityArr.push_back(Gtk::make_managed<LogEntity>());
+    grid.attach_next_to(*logEntityArr.back(), *logEntityArr[logEntityArr.size()-2], Gtk::POS_BOTTOM,3,1);
+    //grid.attach_next_to(btnAdd, *logEntityArr.back(),Gtk::POS_BOTTOM,3);
+
+
+}
 }   // namespace core::ui
--- src/ui/wfrlog.hpp
+++ src/ui/wfrlog.hpp
@@ -2,15 +2,21 @@
 #include <glibmm/ustring.h>
 #include <gtkmm/button.h>
 #include <gtkmm/grid.h>
+#include <vector>
+#include <glibmm/refptr.h>
 #include "logentity.hpp"
 
 namespace core::ui {
+
+    using LogEntityArr = std::vector<LogEntity*>;
 
 class WFrLog final : public Gtk::Frame {
     const Glib::ustring Label { "Logger" };
     Gtk::Button         btnAdd;
     Gtk::Grid           grid;
-    LogEntity           logEntity;
+    LogEntityArr logEntityArr;
+
+    void onBtnClicked();
 
 public:
     WFrLog();
