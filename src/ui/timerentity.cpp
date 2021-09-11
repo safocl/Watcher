dd4756dc9527059a2b925c19d347cbf6e18757dd
safocl <safocl88@gmail.com>
2021-09-11 08:38:41 +0400

    fixed bug for click on start button of timer

--- src/ui/timerentity.cpp
+++ src/ui/timerentity.cpp
@@ -106,7 +106,7 @@ timerPtr(), progressBarDispetcher() {
 TimerEntity::~TimerEntity() {}
 
 void TimerEntity::onButtonClicked() {
-    if ( btn.get_label().c_str() == strStart.c_str() ) {
+    if ( btn.get_label() == strStart.c_str() ) {
         int                  secValue  = spSeconds.get_value_as_int();
         int                  minValue  = spMinutes.get_value_as_int() * 60;
         int                  hourValue = spHours.get_value_as_int() * 3600;
