1d6abd387cf56064246a977fe0eb4964c4e9d19d
safocl <safocl88@gmail.com>
2023-07-12 19:55:07 +0400

    Fix misprint in the license headers.

--- src/ui/logentity.cpp
+++ src/ui/logentity.cpp
@@ -5,19 +5,19 @@
  *\date 2023
  *
  * \detail \"Copyright safocl (megaSafocl) 2023\"
- This file is part of PockerCalc2.
+ This file is part of watcher.
 
- PockerCalc2 is free software: you can redistribute it and/or modify it under
+ watcher is free software: you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation, either version 3 of the License, or any later version.
 
- PockerCalc2 is distributed in the hope that it will be useful, but
+ watcher is distributed in the hope that it will be useful, but
  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  more details.
 
  You should have received a copy of the GNU General Public License along with
- PockerCalc2. If not, see <https://www.gnu.org/licenses/>.
+ watcher. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "logentity.hpp"
